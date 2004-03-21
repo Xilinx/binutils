bbd6a40622a721684ce7904ec752c7947a7e300c
Andrew Cagney <cagney@redhat.com>
2004-03-21 23:57:44 +0000

    Merge with mainline cagney_tramp-20040321-mergepoint.

--- ld/testsuite/ld-scripts/size-2.s
+++
@@ -1,9 +0,0 @@
-	.section .text,"ax",@progbits
-	.long 1,2
-
-	# thread local storage sections
-	.section .tdata,"awT",@progbits
-	.long 6,7,8,9,10
-	
-	.section .tbss,"awT",@nobits
-	.long 0,0,0,0,0,0
