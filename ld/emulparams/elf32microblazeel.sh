ad8fcd55d7c4eb46a00af719c1bc8df70dcde560
Edgar E. Iglesias <edgar.iglesias@gmail.com>
2011-09-20 11:30:46 +0200

    Add support for microblaze little-endian
    Add microblazeel target support.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@gmail.com>
    Signed-off-by: David Holsgrove <david.holsgrove@xilinx.com>

--- ld/emulparams/elf32microblazeel.sh
+++ ld/emulparams/elf32microblazeel.sh
@@ -1,5 +1,5 @@
 SCRIPT_NAME=elfmicroblaze
-OUTPUT_FORMAT="elf32-microblaze"
+OUTPUT_FORMAT="elf32-microblazeel"
 BIG_OUTPUT_FORMAT="elf32-microblaze"
 LITTLE_OUTPUT_FORMAT="elf32-microblazeel"
 #TEXT_START_ADDR=0
@@ -21,5 +21,3 @@ NOP=0x80000000
 
 TEMPLATE_NAME=elf32
 #GENERATE_SHLIB_SCRIPT=yes
-
-    
